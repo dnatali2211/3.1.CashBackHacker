bb54cc1ed41480fdce9985ffa8ae46e6a2744e3e
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-06-15 10:10:48 +0400

    Changed 'remain' in CashbackHackService.java

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,12 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
+        if (amount == 0) {
+            return boundary;
+        } else if (amount % boundary == 0) {
+            return 0;
+        } else {
+            return boundary - amount % boundary;
+        }
     }
 }
