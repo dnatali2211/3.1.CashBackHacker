5fc9a259819bbf239cc8be92ce16f4d60b7748ae
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-06-15 17:19:32 +0400

    Added branch 'junit4-platform'

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class CashbackHackServiceTest {
 
@@ -10,27 +9,27 @@ public class CashbackHackServiceTest {
     public void remainIfBelowBoundary() {
         CashbackHackService service = new CashbackHackService();
 
-        assertEquals(service.remain(900), 100);
+        Assertions.assertEquals(service.remain(900), 100);
     }
 
     @Test
     public void remainIfEqualBoundary() {
         CashbackHackService service = new CashbackHackService();
 
-        assertEquals(service.remain(1000), 0);
+        Assertions.assertEquals(service.remain(1000), 0);
     }
 
     @Test
     public void remainIfZero() {
         CashbackHackService service = new CashbackHackService();
 
-        assertEquals(service.remain(0), 1000);
+        Assertions.assertEquals(service.remain(0), 1000);
     }
 
     @Test
     public void remainIfAboveBoundary() {
         CashbackHackService service = new CashbackHackService();
 
-        assertEquals(service.remain(1567), 433);
+        Assertions.assertEquals(service.remain(1567), 433);
     }
 }
